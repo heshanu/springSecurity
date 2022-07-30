174a3f87bcfe21216bf49252b61473f2b9b74804
heshanu <heshanu97@gmail.com>
2022-07-30 06:04:18 +0530

    Added

--- security/src/main/java/com/example/security/controller/HelloController.java
+++ security/src/main/java/com/example/security/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.security.controller;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
 
+@RestController
+@EnableWebSecurity
+public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
--- security/src/main/java/com/example/security/sec/MysecurityConfig.java
+++ security/src/main/java/com/example/security/sec/MysecurityConfig.java
@@ -1,26 +1,41 @@
 package com.example.security.sec;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class MysecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //super.configure(auth);
-
+        //save user details inmemory
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("tom").password(passwordEncoder.encode("123")).authorities("read").build();
+        userDetailsService.createUser(user);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // super.configure(http);
-        http
-                .httpBasic();
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated();
-
-
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
     }
+
+    /*
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+       */
 }
