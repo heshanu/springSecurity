fd1b2832086781234d63bea12539ae5cde015d95
heshanu <heshanu97@gmail.com>
2022-08-06 05:52:24 +0530

    Added new project

---
+++ coupenService/src/test/java/com/example/coupenService/CoupenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ productSerivce/src/test/java/com/example/productSerivce/ProductSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productSerivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
